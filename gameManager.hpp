0723894128dab9381d8dfc1772be47edd3e56ac4
Dang <3thandang@gmail.com>
2023-10-08 05:12:01 -0400

    changed some game stats and states

--- gameManager.hpp
+++ gameManager.hpp
@@ -3,14 +3,6 @@
 #include "zUI.hpp"
 #include "gameDefs.hpp"
 
-enum class GameState {
-    MainMenu,
-    OptionsMenu,
-    Game,
-    GameOver,
-    End = -1
-};
-
 class GameManager {
 public:
     GameManager(UI& ui) : m_ui(ui) {}
@@ -25,19 +17,35 @@ private:
     std::vector<Zorb*> m_enemyZorbs;    // Enemy zorbs
     std::vector<Zorb*> m_nzorbs;    // npc zorbs
     bool m_isPaused = false;
-    GameState m_gameState = GameState::OptionsMenu;
+    GameState m_gameState = GameState::InfoMenu;
 
+    //region Zorb Management
+    void SortZorbsByTeamId();
+    void GenerateZorb(int team_id);
+    //endregion
+
+    //region Input Handling
     void handleMainMenuInput(char input);
     void handleOptionsMenuInput(char input);
-    void handlePauseMenuInput(char input);
     void handleGameOverInput(char input);
+    //endregion
+
+    //region Stats
+    int m_playerScore = 0;
+    int m_enemyScore = 0;
+    //endregion
 };
 
+//region GameManager Functions
 void GameManager::startGame() {
     // Initialize game objects
-    m_zorbs.push_back(Zorb(1)); // Player zorb
+    //initialize one Zorb object that is a default green zorb named "Neep Narp" that is pushed back into the vector of Zorbs
+    m_zorbs.push_back(Zorb(0, 1, "Neep Narp"));
+    m_zorbs.at(0).SetAppearance(APPEARANCE_DEFAULT);
+
     m_zorbs.push_back(Zorb(2)); // Enemy zorb
     m_zorbs.push_back(Zorb(0)); // Player zorb
+    SortZorbsByTeamId();
 
     // Game loop
     while (true) {
@@ -56,6 +64,15 @@ void GameManager::startGame() {
                 break;
             case GameState::Game:
                 break;
+            case GameState::InfoMenu:
+                _createStyledTextBox("In the furthest reaches of the cosmos, in a galaxy far, far away, there existed a race of adorable yet feisty aliens known as Zorbs. These lovable creatures, resembling a delightful fusion of Earthly cats and fuzzy aliens, lived in a galaxy filled with cuddles, meows, and of course, intergalactic warfare.");
+                _pauseSystem();
+                std::cout << "\n\n" << std::endl;
+                _createStyledTextBox("This is a rogue-like video game that introduces you to the whimsical world of these fluffy aliens. The game's first character, Neep Narp, is a Zorb with the heart of a true hero, and it's your mission to guide Neep Narp and their friends through the cosmic chaos.");
+                std::cout << m_zorbs.at(0);
+                m_ui.screenInfo();
+                m_gameState = GameState::MainMenu;
+                break;
             case GameState::GameOver:
                 m_ui.screenGameOver();
                 char gameOverInput;
@@ -69,13 +86,12 @@ void GameManager::startGame() {
             break;
     }
     _clearScreen();
-    
+
     std::cout << "\n\n\n\n\n" << std::endl;
     std::string asciiArt = "/\\/\\      o      /\\/\\\n\\  /   ./\\|/\\.   \\  /\n \\/    ( ^.^ )    \\/ \n       b> ^ <d       \n";
     _createStyledTextBox(asciiArt+"\n<3 Thank you for playing Zorb Zenith! <3\nMay you attain your own inner zen!\n\n-Dang");
     _pauseSystem();
 }
-
 
 void GameManager::endGame() {
     // Clean up game objects
@@ -86,12 +102,18 @@ void GameManager::endGame() {
     m_enemyZorbs.clear();
 }
 
+//endregion
+
+//region Input Handling
 void GameManager::handleMainMenuInput(char input) {
     switch (input) {
         case '1':
             m_gameState = GameState::Game;
             break;
         case '2':
+            m_gameState = GameState::InfoMenu;
+            break;
+        case '3':
             m_gameState = GameState::OptionsMenu;
             break;
         case 'q':
@@ -128,23 +150,6 @@ void GameManager::handleOptionsMenuInput(char input) {
     }
 }
 
-void GameManager::handlePauseMenuInput(char input) {
-    switch (input) {
-        case '1':
-            m_gameState = GameState::Game;
-            break;
-        case '2':
-            m_gameState = GameState::OptionsMenu;
-            break;
-        case 'q':
-        case 'Q':
-            m_gameState = GameState::GameOver;
-            break;
-        default:
-            break;
-    }
-}
-
 void GameManager::handleGameOverInput(char input) {
     endGame();
     switch (input) {
@@ -158,3 +163,34 @@ void GameManager::handleGameOverInput(char input) {
             break;
     }
 }
+//endregion
+
+//region Zorb Management
+void GameManager::SortZorbsByTeamId() {
+    for (auto& zorb : m_zorbs) {
+        if (zorb.GetTeamId() == 0) {
+            m_playerZorbs.push_back(&zorb);
+        }
+        else if (zorb.GetTeamId() == 1) {
+            m_enemyZorbs.push_back(&zorb);
+        }
+        else {
+            m_nzorbs.push_back(&zorb);
+        }
+    }
+}
+
+//have a function that generates a random zorb
+void GameManager::GenerateZorb(int team_id) {
+    //randomly generate the power for this zorb object
+    //make there a factor on the integer power depending on the amount of wins the player has and the size of the vector of zorbs the player has
+    int power;
+    power = rand() % (5 + m_playerScore) + 1;
+
+    //generate a random zorb
+    Zorb newZorb = Zorb(power, team_id, zorbNames[rand() % zorbNames.size()]+' '+zorbNames[rand() % zorbNames.size()]);
+    //add it to the vector of zorbs
+    m_zorbs.push_back(newZorb);
+}
+
+//endregion
